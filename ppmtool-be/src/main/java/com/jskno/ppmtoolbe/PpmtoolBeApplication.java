ab10dc3ec78f383dda7afc4de14fb55ab02ab420
jskno <lodmnos@gmail.com>
2018-11-10 12:53:38 +0100

    Starting Back End

---
+++ ppmtool-be/src/main/java/com/jskno/ppmtoolbe/PpmtoolBeApplication.java
@@ -0,0 +1,12 @@
+package com.jskno.ppmtoolbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmtoolBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PpmtoolBeApplication.class, args);
+    }
+}
